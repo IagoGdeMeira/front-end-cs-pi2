d2b6503490848842a248c85dc2d34a13f1ca38b6
IagoGdeMeira <iagomeirifpr@gmail.com>
2024-08-26 20:13:10 -0300

    Configurando o ambiente: PrimeReact, páginas e componentes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import CreateTeacher from './pages/createTeacher/CreateTeacher';
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element='' />
+          <Route path='/create_teacher' element={<CreateTeacher/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/createTeacher/CreateTeacher.jsx
@@ -0,0 +1,10 @@
+import './CreateTeacher.css';
+
+
+function CreateTeacher() {
+    return (
+        <h1>Hello, World!</h1>
+    );
+}
+
+export default CreateTeacher;
