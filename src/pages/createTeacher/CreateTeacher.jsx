2e80654d32e33b5958d88a580073dabdff12dd00
IagoGdeMeira <iagomeirifpr@gmail.com>
2024-08-26 20:27:59 -0300

    Ajustando problemas na implementação do React. Funciona corretamente agora.

--- src/pages/createTeacher/CreateTeacher.jsx
+++ src/pages/createTeacher/CreateTeacher.jsx
@@ -1,9 +1,20 @@
 import './CreateTeacher.css';
 
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+
 
 function CreateTeacher() {
+    const [name, setName] = useState('');
+
     return (
-        <h1>Hello, World!</h1>
+        <div className="card flex justify-content-center">
+            <FloatLabel>
+                <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <label for="name">Nome do professor</label>
+            </FloatLabel>
+        </div>
     );
 }
 
