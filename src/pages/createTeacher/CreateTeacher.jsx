ff0287381ab3f422c2a7bcee976b35e8429cd38c
IagoGdeMeira <iagomeirifpr@gmail.com>
2024-08-26 22:27:10 -0300

    Alinhando itens de formulário em linhas. Tem que reposicionar o conteúdo no centro.

--- src/pages/createTeacher/CreateTeacher.jsx
+++ src/pages/createTeacher/CreateTeacher.jsx
@@ -10,27 +10,44 @@ import SimpleLayout from '../../components/simpleLayout/SimpleLayout';
 function CreateTeacher() {
     const [name, setName] = useState('');
     const [cpf, setCPF] = useState('');
+    const [rg, setRG] = useState('');
 
     return (
         <SimpleLayout>
-            <form className="label-gap card flex justify-content-center">
-                <FloatLabel>
-                    <InputText
-                        id="name"
-                        onChange={(e) => setName(e.target.name)}
-                        value={name}
-                    />
-                    <label for="name">Nome do professor</label>
-                </FloatLabel>
-                <FloatLabel>
-                    <InputMask
-                        mask="000.000.000-00"
-                        onChange={(e) => setCPF(e.target.cpf)}
-                        placeholder="000.000.000-00"
-                        value={cpf}    
-                    />
-                    <label for="cpf">CPF do professor</label>
-                </FloatLabel>
+            <form className="card flex justify-content-center">
+                <div className="form-item">
+                    <FloatLabel>
+                        <InputText
+                            className="input-width"
+                            id="name"
+                            onChange={(e) => setName(e.target.name)}
+                            value={name}
+                        />
+                        <label for="name">Nome do professor</label>
+                    </FloatLabel>
+                </div>
+                <div className="form-item">
+                    <FloatLabel>
+                        <InputMask
+                            className="input-width"
+                            mask="XXX.XXX.XXX-XX?"
+                            onChange={(e) => setCPF(e.target.cpf)}
+                            value={cpf}    
+                        />
+                        <label for="cpf">CPF do professor</label>
+                    </FloatLabel>
+                </div>
+                <div className="form-item">
+                    <FloatLabel>
+                        <InputMask
+                            className="input-width"
+                            mask="XX.XXX.XXX-X?"
+                            onChange={(e) => setRG(e.target.rg)}
+                            value={rg}
+                        />
+                        <label for="rg">RG do professor</label>
+                    </FloatLabel>
+                </div>
             </form>
         </SimpleLayout>
     );
