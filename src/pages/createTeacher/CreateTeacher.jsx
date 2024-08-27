ada13b06d2f78129ab0e794fde66f319a5ac5ebc
IagoGdeMeira <iagomeirifpr@gmail.com>
2024-08-26 21:07:08 -0300

    Ajustando o cadastro de professores. Preciso tratar a orientação das labels para não deixar múltiplas labels na mesma linha.

--- src/pages/createTeacher/CreateTeacher.jsx
+++ src/pages/createTeacher/CreateTeacher.jsx
@@ -1,20 +1,38 @@
 import './CreateTeacher.css';
 
 import React, { useState } from "react";
+import { InputMask } from "primereact/inputmask";
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
+import SimpleLayout from '../../components/simpleLayout/SimpleLayout';
 
 
 function CreateTeacher() {
     const [name, setName] = useState('');
+    const [cpf, setCPF] = useState('');
 
     return (
-        <div className="card flex justify-content-center">
-            <FloatLabel>
-                <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
-                <label for="name">Nome do professor</label>
-            </FloatLabel>
-        </div>
+        <SimpleLayout>
+            <form className="label-gap card flex justify-content-center">
+                <FloatLabel>
+                    <InputText
+                        id="name"
+                        onChange={(e) => setName(e.target.name)}
+                        value={name}
+                    />
+                    <label for="name">Nome do professor</label>
+                </FloatLabel>
+                <FloatLabel>
+                    <InputMask
+                        mask="000.000.000-00"
+                        onChange={(e) => setCPF(e.target.cpf)}
+                        placeholder="000.000.000-00"
+                        value={cpf}    
+                    />
+                    <label for="cpf">CPF do professor</label>
+                </FloatLabel>
+            </form>
+        </SimpleLayout>
     );
 }
 
