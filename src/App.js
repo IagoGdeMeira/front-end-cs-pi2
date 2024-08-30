d5c8a3abbd2f1e488996bc7bae8dab81f3c34ce2
GregoryVinicius <gregoryviniciuscla@gmail.com>
2024-08-30 20:58:08 -0300

    Adicionando um componente referente a exclusão

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DisciplineCreate from "./pages/DisciplineCreate/DisciplineCreate";
 import Home from "./pages/Home/Home";
+import DeleteButton from './components/deleteButton/DeleteButton';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/discipline-create' element={<DisciplineCreate/>}/>
+          <Route path='/teste-botao-delete' element={<DeleteButton/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/deleteButton/DeleteButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './DeleteButton.css';
+import "primeicons/primeicons.css";
+
+
+const DeleteButton = () =>{
+
+    return(
+        <div className="layout-button">
+            <i class="pi pi-trash"></i>
+        </div>
+    );
+}
+
+export default DeleteButton;
