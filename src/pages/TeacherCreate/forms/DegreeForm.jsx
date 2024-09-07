a82a36877c07e885824bd4f1b20692a5e68de3c2
IagoGdeMeira <iagomeiraifpr@gmail.com>
2024-09-07 18:08:10 -0300

    Desenvolvendo os campos de graduação. Precisa ser tornado em componente para lidar com múltiplas graduações.

--- src/pages/TeacherCreate/forms/DegreeForm.jsx
+++ src/pages/TeacherCreate/forms/DegreeForm.jsx
@@ -1,13 +1,5 @@
-import { InputMask } from 'primereact/inputmask';
-import { InputText } from 'primereact/inputtext';
 import React, { useState } from "react";
-
-import {
-    handleCourseNameChange,
-    handleConclusionDateChange,
-    handleCourseLocationChange
-} from "../handlers/degreeHandlers";
-
+import Degree from "../../../components/Degree/Degree";
 
 const DegreeForm = () => {
     const [degree, setDegree] = useState({
@@ -19,49 +11,10 @@ const DegreeForm = () => {
     return (
         <section className="border-top-2 border-yellow-800 flex flex-column">
             <h2>Graduações do Professor</h2>
-            <div className="form-row flex">
-                {/* Campo para o NOME DO CURSO da graduação. */}
-                <div className="form-item flex flex-column align-items-start">
-                    <label htmlFor="degreeCourseName">Nome do curso:</label>
-                    <InputText
-                        aria-describedby="degreeCourseName-help"
-                        className="w-full text-overflow-ellipsis"
-                        id="degreeCourseName"
-                        onChange={(e) => handleCourseNameChange(e, degree, setDegree)}
-                        value={degree.degreeCourseName}
-                    />
-                    <small id="degreeCourseName-help">Curso de graduação.</small>
-                </div>
-
-                {/* Campo para a DATA DE CONCLUSÃO da graduação. */}
-                <div className="w-9rem form-item-date flex flex-column align-items-start">
-                    <label htmlFor="degreeConclusionDate">Conclusão:</label>
-                    <InputMask
-                        aria-describedby="degreeConclusionDate-help"
-                        className="w-full text-overflow-ellipsis"
-                        id="degreeConclusionDate"
-                        mask="99/99/9999"
-                        onChange={(e) => handleConclusionDateChange(e, degree, setDegree)}
-                        value={degree.degreeConclusionDate}
-                    />
-                    <small id="degreeConclusionDate-help">Data de conclusão.</small>
-                </div>
-            </div>
-
-            <div className="form-row flex">
-                {/* Campo para o LOCAL da graduação. */}
-                <div className="form-item flex flex-column align-items-start">
-                    <label htmlFor="degreeCourseLocation">Local da Graduação:</label>
-                    <InputText
-                        aria-describedby="degreeCourseLocation-help"
-                        className="w-full text-overflow-ellipsis"
-                        id="degreeCourseLocation"
-                        onChange={(e) => handleCourseLocationChange(e, degree, setDegree)}
-                        value={degree.degreeCourseLocation}
-                    />
-                    <small id="degreeCourseLocation-help">Local onde a graduação foi realizada.</small>
-                </div>
-            </div>
+            <Degree
+                degree={degree}
+                setDegree={setDegree}
+            />
         </section>
     );
 };
