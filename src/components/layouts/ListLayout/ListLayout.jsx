251f5bc9dcf50a72f8533554c8f33c569824094b
IagoGdeMeira <iagomeiraifpr@gmail.com>
2024-11-02 12:43:00 -0300

    Criando arquivos para componentes e layouts de lista. Excluindo componentes obsoletos.

---
+++ src/components/layouts/ListLayout/ListLayout.jsx
@@ -0,0 +1,12 @@
+import './ListLayout.css';
+
+
+const ListLayout = ({ children }) => {
+    return (
+        <main>
+            {children}
+        </main>
+    );
+}
+
+export default ListLayout;
